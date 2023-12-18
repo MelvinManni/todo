c15350ba330b0e9924ae5194a7fe9bbbe1fb1f28
Melvin <melvinnnamani@gmail.com>
2023-12-18 17:02:38 +0100

    improve app block

--- lib/bloc/app_bloc.dart
+++ lib/bloc/app_bloc.dart
@@ -22,18 +22,18 @@ class AppBloc extends Bloc<AppEvent, AppState> {
       emit(AppState(authStatus: AuthStatus.authenticated, user: event.user));
     });
     on<AppLoggedOut>((event, emit) {
-      unawaited(authRepository.signOut());
+      unawaited(_authRepository.signOut());
       emit(const AppState(authStatus: AuthStatus.unauthenticated));
     });
     on<AppTodoItemsRequested>((event, emit) {
       emit(state.copyWith(status: Status.loading));
-      todoRepository.todos(userId: state.user.id).listen((todos) {
+      _todoRepository.todos(userId: state.user.id).listen((todos) {
         emit(state.copyWith(status: Status.success, todos: todos));
       });
     });
     on<AppTodoItemAdded>((event, emit) {
       emit(state.copyWith(status: Status.loading));
-      todoRepository
+      _todoRepository
           .add(userId: state.user.id, todo: event.task)
           .then((value) => {
                 emit(state.copyWith(
@@ -42,7 +42,7 @@ class AppBloc extends Bloc<AppEvent, AppState> {
     });
     on<AppTodoItemDeleted>((event, emit) {
       emit(state.copyWith(status: Status.loading));
-      todoRepository
+      _todoRepository
           .delete(userId: state.user.id, todoId: event.todoItem.id)
           .then((value) => {
                 emit(state.copyWith(
